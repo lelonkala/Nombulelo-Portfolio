807380d012af525c6fde8a8435285b201e16355e
Prudence Nkala <pnkala@network.global>
2026-09-02 15:35:40 +0200

    added a certificate

--- static/javascript/carousel.js
+++ static/javascript/carousel.js
@@ -1,16 +1,19 @@
-function scrollCertificates(direction){
-        const carousel =
+function scrollCertificates(index){
+
+    const carousel =
         document.getElementById("certificateCarousel");
 
-        const scrollAmount = carousel.scrollWidth / 2;
+    const scrollAmount =
+        carousel.clientWidth;
 
-        carousel.srollTo({
-            left: index *scrollAmount,
-            behavior: "smooth"
-        });
+    carousel.scrollTo({
+        left: index * scrollAmount,
+        behavior: "smooth"
+    });
 
-        document.querySelectorAll(".dot")
-            .forEach(dot => dot.classList.remove("active"));
-        document.querySelectorAll(".dot")[index]
-            .classList.add("active");
-    };
+    document.querySelectorAll(".dot")
+        .forEach(dot => dot.classList.remove("active"));
+
+    document.querySelectorAll(".dot")[index]
+        .classList.add("active");
+}
