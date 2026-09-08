9f3e58cd1e092266a7084d8b495a170793df2b00
Prudence Nkala <pnkala@network.global>
2026-09-08 11:59:08 +0200

    Carousel updated, with it's javascript

--- static/javascript/carousel.js
+++ static/javascript/carousel.js
@@ -1,19 +1,58 @@
-function scrollCertificates(index){
+document.addEventListener("DOMContentLoaded", () => {
 
-    const carousel =
-        document.getElementById("certificateCarousel");
+    const carousel = document.getElementById("certificateCarousel");
+    const nextBtn = document.getElementById("nextBtn");
+    const prevBtn = document.getElementById("prevBtn");
 
-    const scrollAmount =
-        carousel.clientWidth;
+    if (!carousel ) {
+        console.error("Carousel elements not found.");
+        return;
+    }
+    if(!prevBtn){
+        console.error("Previous button not found.");
+        return;
+    }
+    if(!nextBtn){
+        console.error("Next Button not found");
+        return;
+    }
+    function updateButtons(){
+        const maxScroll =  carousel.scrollWidth - carousel.parentElement.offsetWidth;
 
-    carousel.scrollTo({
-        left: index * scrollAmount,
-        behavior: "smooth"
+        prevBtn.disabled = currentPosition >= 0;
+        nextBtn.disabled = Math.abs(currentPosition) >= maxScroll;
+    }
+
+    let currentPosition = 0;
+
+    nextBtn.addEventListener("click", function() {
+
+        carousel.style.animationPlayState = "paused";
+        const maxScroll = carousel.scrollWidth - carousel.parentElement.offsetWidth;
+        if (Math.abs(currentPosition) < maxScroll){
+            currentPosition -= 300;
+
+             carousel.style.transform =
+            "translateX("  + currentPosition + "px)";
+        }
+
+        updateButtons();
     });
 
-    document.querySelectorAll(".dot")
-        .forEach(dot => dot.classList.remove("active"));
+    prevBtn.addEventListener("click", function() {
 
-    document.querySelectorAll(".dot")[index]
-        .classList.add("active");
-}
+        
+        carousel.style.animationPlayState = "paused";
+        if (currentPosition < 0 ){
+            currentPosition += 300;
+
+            carousel.style.transform =
+            "translateX(" + currentPosition + "px)";
+
+        }
+
+       updateButtons();
+
+    });
+
+});
